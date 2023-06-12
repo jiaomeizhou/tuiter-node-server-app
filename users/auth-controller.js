2b22c03023c0b6f3eb6a05a21bfc1f1ed354d702
jiaomeizhou <zhoujiaomei@outlook.com>
2023-06-12 10:37:44 -0700

    first commit

---
+++ users/auth-controller.js
@@ -0,0 +1,61 @@
+
+import * as usersDao from "./users-dao.js";
+
+var currentUserVar;
+const AuthController = (app) => {
+    const register = async (req, res) => {
+        const username = req.body.username;
+        const user = await usersDao.findUserByUsername(username);
+        if (user) {
+            res.sendStatus(409);
+            return;
+        }
+        const newUser = await usersDao.createUser(req.body);
+        currentUserVar = newUser;
+        res.json(newUser);
+    };
+
+    const login = async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = await usersDao.findUserByCredentials(username, password);
+        if (user) {
+            currentUserVar = user;
+            res.json(user);
+        } else {
+            res.sendStatus(404);
+        }
+    };
+
+    const profile = async (req, res) => {
+        const currentUser = currentUserVar
+        if (!currentUser) {
+            res.sendStatus(404);
+            return;
+        }
+        res.json(currentUser);
+    };
+
+    const logout = async (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+
+    const update = (req, res) => { };
+
+
+    app.post("/api/users/register", register);
+    app.post("/api/users/login", login);
+    app.post("/api/users/profile", profile);
+    app.post("/api/users/logout", logout);
+    app.put("/api/users", update);
+};
+export default AuthController;
+
+// //TODO
+// const update   = (req, res) => {
+//     const currentUser = req.session["currentUser"];
+//     const user = usersDao.updateUser(req.body, currentUser);
+//     req.session["currentUser"] = user;
+//     res.json(user);
+// };
