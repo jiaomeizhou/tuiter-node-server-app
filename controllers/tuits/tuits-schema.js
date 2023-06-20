0d8bcad365aaafdfab7de81c96c3e1e2850ee204
jiaomeizhou <zhoujiaomei@outlook.com>
2023-06-20 08:17:04 -0700

    Modify tuit schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+    time: String,
+    topic: String,
     tuit: String,
     likes: Number,
     liked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    username: String,
+    image: String,
+    handle: String,
 }, {collection: 'tuits'});
 export default schema;
